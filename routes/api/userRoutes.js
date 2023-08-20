ba8408fbc475287cad380d9a61ed489cea18de5d
ricguer <rj.guerra@outlook.com>
2023-08-20 12:02:54 -0400

    Define User Routes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,27 @@
+                                                                /* ===================== IMPORTS ====================== */
+const router = require("express").Router();
+
+const {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser
+} = require("../../controllers/userController");
+
+
+                                                                /* ===================== ROUTES ======================= */
+router.route("/").get(getAllUsers).post(createUser);            /* /api/users                                           */
+
+router.route("/:userId").get(getUserById);                      /* /api/users/:userId                                   */
+
+router.route("/:userId").put(updateUser);                       /* /api/users/:userId/friends/:friendId                 */
+
+router.route("/:userId").delete(deleteUser);                    /* /api/users/:userId/friends/:friendId                 */
+
+                                                                /* /api/users/:userId/friends/:friendId                 */
+router.route("/:userId/friends/:friendId").post(addFriend).delete(deleteFriend);
+
+
+                                                                /* ==================== EXPORT ======================= */
+module.exports = router;
