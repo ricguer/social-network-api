7dc9cddb7f6af25b99afa40e87913236694e61fa
ricguer <rj.guerra@outlook.com>
2023-08-24 23:17:51 -0400

    Rewrite Seed File to Include Username, Email, and Thought Arrays

--- utils/seed.js
+++ utils/seed.js
@@ -1,7 +1,3 @@
-[
-
-]
-
 const connection = require("../config/connection");
 const { User, Thought } = require("../models");
 
@@ -11,91 +7,69 @@ connection.once("open", async () => {
     await User.deleteMany({});
     await Thought.deleteMany({});
 
-    const users = await User.insertMany([
-          {
-            username: "lernantino",
-            email: "lernantino@gmail.com"
-          },
-          {
-            username: "codingmaster",
-            email: "codingmaster@example.com"
-          },
-          {
-            username: "techgeek123",
-            email: "techgeek123@gmail.com"
-          },
-          {
-            username: "designguru",
-            email: "designguru@example.com"
-          },
-          {
-            username: "gamer123",
-            email: "gamer123@gmail.com"
-          },
-          {
-            username: "webdevpro",
-            email: "webdevpro@example.com"
-          },
-          {
-            username: "musiclover22",
-            email: "musiclover22@gmail.com"
-          },
-          {
-            username: "fitnessfanatic",
-            email: "fitnessfanatic@example.com"
-          },
-          {
-            username: "bookworm99",
-            email: "bookworm99@gmail.com"
-          },
-          {
-            username: "travelbug45",
-            email: "travelbug45@example.com"
-          }
-    ]);
+    const usernames = [
+      "lernantino" ,
+      "codingmaster" ,
+      "techgeek123" ,
+      "designguru" ,
+      "gamer123" ,
+      "webdevpro" ,
+      "musiclover22" ,
+      "fitnessfanatic" ,
+      "bookworm99" ,
+      "travelbug45"
+    ];
 
-    const thoughts = await Thought.insertMany([
-        {
-            thoughtText: "Here's a cool thought...",
-            username: "lernantino"
-        },
-        {
-            thoughtText: "I like to code",
-            username: "codingmaster"
-        },
-        {
-            thoughtText: "I like technology",
-            username: "techgeek123"
-        },
-        {
-            thoughtText: "I like to design things",
-            username: "designguru"
-        },
-        {
-            thoughtText: "I like to play video games",
-            username: "gamer123"
-        },
-        {
-            thoughtText: "I like to develop websites",
-            username: "webdevpro"
-        },
-        {
-            thoughtText: "I like to listen to music",
-            username: "musiclover22"
-        },
-        {
-            thoughtText: "I like to workout",
-            username: "fitnessfanatic"
-        },
-        {
-            thoughtText: "I like to read books",
-            username: "bookworm99"
-        },
-        {
-            thoughtText: "I like to travel",
-            username: "travelbug45"
-        }
-    ]);
+    const emails = [
+      "lernantino@gmail.com",
+      "codingmaster@example.com",
+      "techgeek123@gmail.com",
+      "designguru@example.com",
+      "gamer123@gmail.com",
+      "webdevpro@example.com",
+      "musiclover22@gmail.com",
+      "fitnessfanatic@example.com",
+      "bookworm99@gmail.com",
+      "travelbug45@example.com"
+    ];
+
+    const thoughts = [
+      "Here's a cool thought..." ,
+      "I like to code" ,
+      "I like technology" ,
+      "I like to design things" ,
+      "I like to play video games" ,
+      "I like to develop websites" ,
+      "I like to listen to music" ,
+      "I like to workout" ,
+      "I like to read books" ,
+      "I like to travel"
+    ];
+
+    // Use the "usernames" and "emails" arrays to create documents in the "User" collection
+    for (let i = 0; i < usernames.length; i++) {
+      const user = usernames[i];
+      const email = emails[i];
+      await User.create({
+          username: user,
+          email: email
+      });
+    }
+
+    // Insert thoughts into each user document
+    for (let i = 0; i < thoughts.length; i++) {
+      const thought = thoughts[i];
+      const user = usernames[i];
+      await Thought.create({
+          thoughtText: thought,
+          username: user
+      });
+      await User.findOneAndUpdate(
+          { username: user },
+          { $push: { thoughts: await Thought.findOne({ username: user }) } },
+          { runValidators: true, new: true }
+      );
+    }
 
     console.log("Seeding complete! 🌱");
     process.exit(0);
